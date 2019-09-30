c9657214c0ba687bb83966ae0e7ef4c23a461792
Qingyun Lin <lin.qin@husky.neu.edu>
2019-09-30 12:57:43 -0700

    assignment3

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -1,9 +1,13 @@
+import React from 'react'
 <!DOCTYPE html>
 <html lang="en">
  <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Title</title>
  </head>
+ <div>datetime = "2018-11-22"</div>
   <body>
       <header>
             <h1>The Status of Liberty's torch heads to new museum</h1>
