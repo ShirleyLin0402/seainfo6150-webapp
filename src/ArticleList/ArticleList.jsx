66bfa5f0fa88f83761ab80656a53011870907376
Qingyun Lin <lin.qin@husky.neu.edu>
2019-10-01 14:29:05 -0700

    assignemt4

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import PropTypes from 'prop-types'
+import ArticleListItem from"./ArticleListItem.jsx"
 
 const ArticleList = props => {
   return (
     <ul>
-      {props.articles.map(article => (
-        <li key={article.slug}>ArticleListItem component goes here</li>
-      ))}
+    {props.articles.map(article =>(
+      <li key={article.slug}>
+      {<ArticleListItem article ={article}/>}</li>
+    ) )}
     </ul>
   );
 };
 
-ArticleList.propTypes = {
-  articles: PropTypes.array.isRequired
-};
+
 export default ArticleList;
